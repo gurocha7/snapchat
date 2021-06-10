3e56a82cd15daf8be8230f0d3f069940f232df64
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-06-09 22:31:18 -0300

    🧩: signup controller created

---
+++ Snapchat/Snapchat/Signup/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  Snapchat
+//
+//  Created by Gustavo Rocha on 09/06/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
--- Snapchat/Snapchat/ViewController.swift
+++ Snapchat/Snapchat/ViewController.swift
@@ -9,20 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - IBOutlets
     
     @IBAction func buttonLoginAction(_ sender: Any) {
         openLogin()
     }
     
+    @IBAction func buttonSignupAction(_ sender: Any) {
+        openSignUP()
+    }
+    
+    //MARK: - Overriddes
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    //MARK: - Methods
     
     private func openLogin() {
         let controller = LoginViewController()
         navigationController?.pushViewController(controller, animated: true)
     }
 
+    private func openSignUP() {
+        let controller = SignUpViewController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
 
 }
 
